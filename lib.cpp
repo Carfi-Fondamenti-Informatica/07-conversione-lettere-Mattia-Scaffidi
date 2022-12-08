0366d804ead739defb6d4315def80272021792a2
Mattia-Scaffidi <116498876+Mattia-Scaffidi@users.noreply.github.com>
2022-12-08 16:04:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,18 +1,24 @@
 #include "lib.h"
-bool convertitore(char &l){
 
-    
 
-        if (l >= 'A' && l <= 'Z') {
-            l = l + 32;
+bool conversione(char &c) {
+
+    if ((c >= 0) && (c <= 9)) {
+        return false;
+
+
+    }
+    else {
+
+
+        if (c >= 'A' && c <= 'Z') {
+            c = c + 32;
+            return true;
+
+        } else if ((c >= 'a') && (c <= 'z')) {
+            c = c - 32;
             return true;
         }
-            else if (l >= 'a' && l <= 'z'){
-                l = l - 32;}
+    }
 
-            return true;
-
-    
-    else  if (l >= 0 && l <= 9)
-        return false;
 }
